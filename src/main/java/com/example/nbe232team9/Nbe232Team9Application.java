4e63967579460a23f621a601fda3f6c8be23ce56
양한동 <yhd0121@gs.anyang.ac.kr>
2024-12-16 14:24:41 +0900

    first commit

---
+++ src/main/java/com/example/nbe232team9/Nbe232Team9Application.java
@@ -0,0 +1,13 @@
+package com.example.nbe232team9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe232Team9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe232Team9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nbe232team9/Nbe232Team9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbe232team9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe232Team9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
