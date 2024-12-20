34f4c232ae5e0c8bdfb75e50676aa0a5af3c8b03
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2024-12-20 17:46:58 +0900

    Refactor: GlobalResponseBodyAdvice 공통응답

--- src/main/java/com/team9/anicare/common/exception/CustomException.java
+++ src/main/java/com/team9/anicare/common/exception/CustomException.java
@@ -1,8 +1,6 @@
 package com.team9.anicare.common.exception;
 
-import com.team9.anicare.common.ResultCode;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 public class CustomException extends RuntimeException{
--- src/main/java/com/team9/anicare/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/team9/anicare/common/exception/GlobalExceptionHandler.java
@@ -1,14 +1,10 @@
 package com.team9.anicare.common.exception;
 
 
-import com.team9.anicare.common.Result;
-import com.team9.anicare.common.ResultCode;
-import org.springframework.http.HttpStatus;
+import com.team9.anicare.common.response.Result;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
