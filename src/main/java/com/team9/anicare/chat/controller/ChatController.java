24cd6515a21a06355003633d24088619fd7d5db9
soyeon1806 <ksy1118s@naver.com>
2024-12-28 13:47:51 +0900

    feat: 채팅방 검색, 퇴장 기능 추가

--- src/main/java/com/team9/anicare/chat/controller/ChatController.java
+++ src/main/java/com/team9/anicare/chat/controller/ChatController.java
@@ -1,13 +1,17 @@
 package com.team9.anicare.chat.controller;
 
 import com.team9.anicare.chat.dto.ChatMessageDTO;
+import com.team9.anicare.chat.dto.ChatRoomDTO;
+import com.team9.anicare.chat.service.ChatLogService;
 import com.team9.anicare.chat.service.ChatRoomService;
 import com.team9.anicare.chat.service.RedisMessagePublisher;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +21,7 @@ public class ChatController {
     private final ChatRoomService chatRoomService;
     private final SimpMessagingTemplate messagingTemplate;
     private final ObjectMapper objectMapper;
+    private final ChatLogService chatLogService;
 
     /**
      * WebSocket 메시지 처리
@@ -53,4 +58,25 @@ public class ChatController {
         );
     }
 
+    // 키워드를 사용한 채팅방 검색
+    @GetMapping("/chat/search")
+    public List<ChatRoomDTO> searchChatRooms(@RequestParam String keyword) {
+        return chatRoomService.searchRooms(keyword);
+    }
+
+    // 채팅 로그 조회
+    @GetMapping("/rooms/{roomId}/logs")
+    public List<String> getChatLogs(@PathVariable String roomId) {
+        return chatLogService.getChatLogs(roomId);
+    }
+
+    // 사용자 퇴장 처리
+    @PostMapping("/rooms/{roomId}/exit")
+    public void exitChatRoom(@PathVariable String roomId, @RequestParam boolean isDoctor) {
+        if (isDoctor) {
+            chatRoomService.handleDoctorExit(roomId);
+        } else {
+            chatRoomService.handleUserExit(roomId);
+        }
+    }
 }
