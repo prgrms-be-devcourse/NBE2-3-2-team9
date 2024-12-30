08892da853121e7045a097867c9a069f1fca8a6a
soyeon <ksy1118s@naver.com>
2024-12-30 14:40:08 +0900

    refactor: 주석 추가

--- src/main/java/com/team9/anicare/chat/config/WebSocketConfig.java
+++ src/main/java/com/team9/anicare/chat/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.team9.anicare.chat.config;
 
 import com.team9.anicare.auth.security.JwtTokenProvider;
 import com.team9.anicare.chat.handler.WebSocketChatHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -10,24 +11,18 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
- * WebSocketConfig 클래스는 WebSocket 메시지 브로커를 구성합니다.
- * - STOMP 프로토콜을 사용하여 WebSocket 연결을 설정합니다.
- * - 클라이언트와 서버 간의 메시지 교환 경로를 정의합니다.
+ * WebSocketConfig 클래스
+ * - STOMP 기반 WebSocket 메시지 브로커 설정
  */
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final JwtTokenProvider jwtTokenProvider;
-
-    public WebSocketConfig(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
-
     /**
-     * STOMP 엔드포인트를 등록하여 클라이언트가 WebSocket에 연결할 수 있도록 설정합니다.
+     * 클라이언트 연결을 위한 WebSocket 엔드포인트 등록
      *
-     * @param registry StompEndpointRegistry 객체
+     * @param registry STOMP 엔드포인트 레지스트리
      */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -38,10 +33,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     /**
-     * 메시지 브로커 구성을 설정합니다.
-     * - 메시지 구독 및 송신 경로를 정의합니다.
+     * 메시지 브로커 설정
+     * - /topic 경로는 클라이언트가 메시지를 구독하는 경로
+     * - /app 경로는 클라이언트가 서버로 메시지를 송신하는 경로
      *
-     * @param registry MessageBrokerRegistry 객체
+     * @param registry 메시지 브로커 레지스트리
      */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -51,8 +47,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
     }
 
+
+    /**
+     * 클라이언트 Inbound 채널 인터셉터 등록
+     * - StompChannelInterceptor를 사용하여 JWT 인증 처리
+     *
+     * @param registration 채널 등록 객체
+     */
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(new StompChannelInterceptor(jwtTokenProvider));
+        // registration.interceptors(new StompChannelInterceptor(jwtTokenProvider));
     }
 }
