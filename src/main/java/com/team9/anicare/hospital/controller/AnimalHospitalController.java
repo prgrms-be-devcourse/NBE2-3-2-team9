a0b4d10bd6eac7fbed4c25b280792ab101cb3bf6
ShinJeongBeom <dingfan36@gmail.com>
2025-01-05 17:57:28 +0900

    feat swagger 설명추가

--- src/main/java/com/team9/anicare/hospital/controller/AnimalHospitalController.java
+++ src/main/java/com/team9/anicare/hospital/controller/AnimalHospitalController.java
@@ -6,6 +6,7 @@ import com.team9.anicare.hospital.dto.AnimalHospitalDetailsDto;
 import com.team9.anicare.hospital.dto.AnimalHospitalDto;
 import com.team9.anicare.hospital.model.AnimalHospital;
 import com.team9.anicare.hospital.service.AnimalHospitalService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
 
 
 @RestController
@@ -27,13 +28,15 @@ import java.util.stream.Collectors;
 public class AnimalHospitalController {
 
     private final AnimalHospitalService animalHospitalService;
-
+    @Operation(summary = "공공 API 호출",
+            description = "공공 API 데이터를 받아옵니다.")
     @GetMapping("/api/animal-hospitals")
     public String importData() {
         animalHospitalService.fetchAndSaveData();
         return "데이터 수집 및 저장 완료";
     }
-
+    @Operation(summary = "좌표 변환",
+            description = "좌표를 변환합니다. EPSG:5179 형식(국내에서 흔히 사용되는 TM 좌표계)일 경우, 이를 EPSG:4326 형식(세계 표준 위경도 좌표계)")
     @PostMapping("/api/update-coordinates")
     public String updateCoordinates() {
         animalHospitalService.fetchAndSaveCoordinates();
@@ -43,6 +46,8 @@ public class AnimalHospitalController {
 
 
     // 특정 위치에서 2km 이내의 동물병원 찾기
+    @Operation(summary = "주변 동물병원 검색",
+            description = "주변 동물병원 검색을 하는 API 입니다. 요청 항목 : latitude, longitude")
     @GetMapping("/api/animal-hospitals/nearby")
     public ResponseEntity<Map<String, Object>> findNearbyHospitals(
             @RequestParam double latitude,
@@ -57,6 +62,8 @@ public class AnimalHospitalController {
     }
 
     //"동물병원"을 검색할경우 동물병원 리스트가 나옴
+    @Operation(summary = "동물병원 단어 검색",
+            description = "동물병원을 검색 하는 API 입니다. 요청 항목: keyword 동물병원")
     @GetMapping("/api/animal-hospitals/namesearch")
     public ResponseEntity<?> searchHospitals(@RequestParam(required = false) String keyword) {
 
@@ -71,6 +78,8 @@ public class AnimalHospitalController {
     }
 
     //  주소 검색 로직
+    @Operation(summary = "동물병원 주소 동, 구, 키워드 검색",
+            description = "동물병원 주소 동, 구, 키워드 검색 API 입니다. 요청 항목 : **동(dong), **구(gu), 키워드(keyword)")
     @GetMapping("/api/animal-hospitals/dongorgu")
     public List<AnimalHospitalDto> searchHospitalsByDongOrGu(
             @RequestParam(required = false) String gu,
@@ -91,7 +100,8 @@ public class AnimalHospitalController {
     }
 
 
-
+    @Operation(summary = "동물병원 좋아요 추가",
+            description = "동물병원을 추가 하는 API 입니다. 요청 항목 : mgtNo:동물병원 고유번호, userId:사용자 ID")
     @PostMapping("/api/animal-hospitals/{mgtNo}/like")
     public ResponseEntity<Result<String>> likeHospital(
             @PathVariable String mgtNo,
@@ -105,6 +115,8 @@ public class AnimalHospitalController {
     }
 
     // 동물병원 좋아요 취소
+    @Operation(summary = "동물병원 좋아요 취소",
+            description = "동물병원을 취소 하는 API 입니다. 요청 항목 : mgtNo:동물병원 고유번호, userId:사용자 ID")
     @DeleteMapping("/api/animal-hospitals/{mgtNo}/like")
     public ResponseEntity<Result<String>> unlikeHospital(
             @PathVariable String mgtNo,
@@ -118,6 +130,8 @@ public class AnimalHospitalController {
     }
 
     // 좋아요 수 확인 API
+    @Operation(summary = "동물병원 좋아요 수 확인",
+            description = "동물병원 좋아요 수 확인 API 입니다. 요청 항목 : mgtNo:동물병원 고유번호")
     @GetMapping("/api/animal-hospitals/{mgtNo}/like-count")
     public ResponseEntity<Result<Long>> getLikeCount(@PathVariable String mgtNo) {
         long likeCount = animalHospitalService.getLikeCount(mgtNo);
@@ -125,6 +139,8 @@ public class AnimalHospitalController {
     }
 
     //동물병원 상세검색
+    @Operation(summary = "동물병원 상세 검색",
+            description = "동물병원 검색을 하면 동물병원 정보가 나옵니다. 요청 항목 : mgtNo:동물병원 고유번호")
     @GetMapping("/api/animal-hospitals/{mgtNo}/details")
     public ResponseEntity<Result<AnimalHospitalDetailsDto>> getHospitalDetails(@PathVariable String mgtNo) {
         AnimalHospitalDetailsDto hospitalDetails = animalHospitalService.getHospitalDetailsDto(mgtNo);
