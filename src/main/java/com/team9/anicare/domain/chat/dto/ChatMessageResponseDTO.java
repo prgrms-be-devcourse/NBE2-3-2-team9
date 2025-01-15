9218652f4fbc959aa4c72e6a4d2ddde7f17b1587
soyeon1806 <ksy1118s@naver.com>
2025-01-16 04:45:07 +0900

    refactor: roomId 필드 추가

--- src/main/java/com/team9/anicare/domain/chat/dto/ChatMessageResponseDTO.java
+++ src/main/java/com/team9/anicare/domain/chat/dto/ChatMessageResponseDTO.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 public class ChatMessageResponseDTO {
 
     private Long messageId;
+    private String roomId;
     private String senderName; // 발신자 이름 (관리자, 유저 닉네임, SYSTEM)
     private String content;
     private ChatMessage.MessageType type;
@@ -21,11 +22,12 @@ public class ChatMessageResponseDTO {
     /**
      * 시스템 메시지 응답 생성 메서드
      */
-    public static ChatMessageResponseDTO systemMessage(String content, ChatMessage.MessageType type) {
+    public static ChatMessageResponseDTO systemMessage(String content, ChatMessage.MessageType type, String roomId) {
         return ChatMessageResponseDTO.builder()
                 .senderName("SYSTEM")  // 발신자 이름을 "SYSTEM"으로 고정
                 .content(content)
                 .type(type)
+                .roomId(roomId)
                 .sentAt(LocalDateTime.now())
                 .build();
     }
