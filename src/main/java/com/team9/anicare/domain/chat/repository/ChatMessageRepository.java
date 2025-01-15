4186470243e0f641d9fab64be1d6ee926a311f7a
soyeon1806 <ksy1118s@naver.com>
2025-01-16 03:51:06 +0900

    refactor: 불필요한 import 제거 및 메시지 내용 검색 쿼리 추가

--- src/main/java/com/team9/anicare/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/team9/anicare/domain/chat/repository/ChatMessageRepository.java
@@ -4,6 +4,8 @@ import com.team9.anicare.domain.chat.entity.ChatMessage;
 import com.team9.anicare.domain.chat.entity.ChatRoom;
 import com.team9.anicare.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +15,10 @@ import java.util.List;
  */
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    // 메시지 내용에 키워드가 포함된 채팅방 ID 조회
+    @Query("SELECT DISTINCT m.chatRoom.roomId FROM ChatMessage m WHERE m.content LIKE %:keyword%")
+    List<String> findDistinctChatRoomIdsByKeyword(@Param("keyword") String keyword);
 
     // 특정 채팅방의 모든 메시지 조회 (최신순)
     List<ChatMessage> findByChatRoomOrderBySentAtAsc(ChatRoom chatRoom);
