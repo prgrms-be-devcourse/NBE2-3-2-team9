5d0488474891b9ccdc3f3f2fb0d0a5e102c40029
soyeon1806 <ksy1118s@naver.com>
2025-01-16 09:30:30 +0900

    refactor: 불필요한 코드 제거

--- src/main/java/com/team9/anicare/domain/chat/config/RedisConfig.java
+++ src/main/java/com/team9/anicare/domain/chat/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.team9.anicare.domain.chat.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.team9.anicare.domain.chat.service.ChatLogService;
 import com.team9.anicare.domain.chat.service.RedisMessageSubscriber;
 import lombok.RequiredArgsConstructor;
 
@@ -22,11 +21,12 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 /**
  * RedisConfig 클래스
- * - Redis의 Pub/Sub 설정, RedisTemplate 직렬화 및 ListenerContainer 설정을 관리합니다.
+ * - Redis의 Pub/Sub 설정, RedisTemplate 직렬화 및 ListenerContainer 설정을 관리함
  */
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
+
     @Value("${spring.data.redis.host}")
     private String redisHost;
 
@@ -40,46 +40,9 @@ public class RedisConfig {
     private String redisChannelTopic;
 
 
-
-
     private final ObjectMapper objectMapper;
     private final SimpMessagingTemplate messagingTemplate;
 
-
-    /**
-     * RedisMessageListenerContainer 설정
-     * - Redis Pub/Sub 메시지를 수신하고 MessageListener로 전달합니다.
-     *
-     * @param connectionFactory Redis 연결 팩토리
-     * @param redisMessageSubscriberAdapter 메시지 리스너 어댑터
-     * @param topic Pub/Sub 채널 주제
-     * @return RedisMessageListenerContainer 빈
-     */
-    @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(
-            RedisConnectionFactory connectionFactory,
-            MessageListenerAdapter redisMessageSubscriberAdapter,
-            ChannelTopic topic) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(redisMessageSubscriberAdapter, topic);
-        return container;
-    }
-
-    /**
-     * RedisMessageSubscriber를 MessageListenerAdapter로 래핑
-     * - handleMessage 메서드를 호출하도록 어댑터 설정
-     *
-     * @return MessageListenerAdapter 빈
-     */
-    @Bean
-    public MessageListenerAdapter redisMessageSubscriberAdapter(
-            ObjectMapper objectMapper,
-            SimpMessagingTemplate messagingTemplate,
-            ChatLogService chatLogService) {
-        RedisMessageSubscriber redisMessageSubscriber = new RedisMessageSubscriber(objectMapper, messagingTemplate, chatLogService);
-        return new MessageListenerAdapter(redisMessageSubscriber, "handleMessage");
-    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
@@ -102,50 +65,75 @@ public class RedisConfig {
      */
     @Bean
     public ChannelTopic topic() {
-        return new ChannelTopic("chatroom");
+        return new ChannelTopic(redisChannelTopic);
     }
+
 
     /**
-     * RedisTemplate 설정
-     * - StringRedisSerializer를 키에 사용하고, 값은 Jackson2JsonRedisSerializer로 직렬화 설정
+     * RedisMessageListenerContainer 설정
+     * - Redis Pub/Sub 메시지를 수신하고 MessageListener로 전달합니다.
      *
      * @param connectionFactory Redis 연결 팩토리
-     * @return RedisTemplate 빈
+     * @param redisMessageSubscriberAdapter 메시지 리스너 어댑터
+     * @param topic Pub/Sub 채널 주제
+     * @return RedisMessageListenerContainer 빈
+     */
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisConnectionFactory connectionFactory,
+            MessageListenerAdapter redisMessageSubscriberAdapter,
+            ChannelTopic topic) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(redisMessageSubscriberAdapter, topic);
+        return container;
+    }
+
+
+    /**
+     * RedisMessageSubscriber를 MessageListenerAdapter로 래핑
+     * - handleMessage 메서드를 호출하도록 어댑터 설정
+     *
+     * @return MessageListenerAdapter 빈
+     */
+    @Bean
+    public MessageListenerAdapter redisMessageSubscriberAdapter() {
+        RedisMessageSubscriber redisMessageSubscriber = new RedisMessageSubscriber(objectMapper, messagingTemplate);        return new MessageListenerAdapter(redisMessageSubscriber, "handleMessage");
+    }
+
+
+    /**
+     * 채팅 메시지 RedisTemplate
+     * - ChatMessageDTO를 직렬화하여 Redis에 저장/조회
      */
     @Bean(name = "chatMessageRedisTemplate")
-    public RedisTemplate<String, ChatMessageDTO> chatMessageRedisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, ChatMessageDTO> template = new RedisTemplate<>();
+    public RedisTemplate<String, Object> chatMessageRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessageDTO.class));
-        template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessageDTO.class));
-        return template;
-    }
-
-    @Bean(name = "chatLogRedisTemplate")
-    public RedisTemplate<String, ChatMessageDTO> chatLogRedisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, ChatMessageDTO> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessageDTO.class));
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessageDTO.class));
-        return template;
-    }
-
-    @Bean
-    public RedisTemplate<String, ChatRoomDTO> chatRoomRedisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, ChatRoomDTO> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatRoomDTO.class));
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(ChatRoomDTO.class));
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
         return template;
     }
 
 
+    /**
+     * 채팅방 RedisTemplate
+     * - ChatRoomDTO를 직렬화하여 Redis에 저장/조회
+     */
+    @Bean(name = "chatRoomRedisTemplate")
+    public RedisTemplate<String, Object> chatRoomRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        return template;
+    }
+
+
+    /**
+     * 기본 RedisTemplate
+     * - 다양한 객체를 저장할 수 있는 범용 RedisTemplate
+     */
     @Bean(name = "defaultRedisTemplate")
     public RedisTemplate<String, Object> defaultRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
