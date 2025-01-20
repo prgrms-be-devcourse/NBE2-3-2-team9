5478480b695b032ccd8f9caa2c15a0c908c3ab4d
soyeon1806 <ksy1118s@naver.com>
2025-01-20 11:57:30 +0900

    fix: 채팅방 검색 프론트 테스트 코드 연결 및 불필요한 조건 검색 수정

--- src/main/java/com/team9/anicare/config/DataLoader.java
+++ src/main/java/com/team9/anicare/config/DataLoader.java
@@ -2,7 +2,6 @@ package com.team9.anicare.config;
 
 import com.team9.anicare.domain.user.model.User;
 import com.team9.anicare.domain.user.repository.UserRepository;
-import com.team9.anicare.domain.user.model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
